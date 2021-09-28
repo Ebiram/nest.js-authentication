0db091eacfc44205e990f2c05e75efe2b3c7e0c8
Ebiram <ebiramgs@live.com>
2021-09-28 15:52:34 +0330

    install packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.use(helmet());
+  app.enableCors({
+    origin: 'http://127.0.0.1:3000',
+    credentials: true,
+  });
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(csurf({ cookie: true }));
   await app.listen(3000);
 }
 bootstrap();
